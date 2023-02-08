330819cee4cfaea626e90d57b4a61c42486bad44
Chris Primes <chris@chrisprimes.com>
2023-02-07 19:36:46 -0500

    Update action

--- forge.config.js
+++ forge.config.js
@@ -19,4 +19,16 @@ module.exports = {
       config: {},
     },
   ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'chrisprimes',
+          name: 'demo-electron-app'
+        },
+        draft: true
+      }
+    }
+  ]
 };
